a2ebe4bc595cfe7871b1bc3b1b99843625a347f8
Emmie Smith <git@emmie.tech>
2026-08-08 23:59:28 -0400

    Initial implementation calculate_damage().

--- fondue/fondue_score/move.py
+++ fondue/fondue_score/move.py
@@ -2,8 +2,17 @@ from .api import fetch_move_info
 from .crit import get_crit_chance
 
 
-def calculate_damage(generation: int, move_url: str) -> float:
+def calculate_damage(generation: int, move_url: str) -> float | None:
     move = fetch_move_info(move_url)
+    power = move["power"]
+
+    # Skip moves less than 50 BP
+    if power < 50:
+        return None
+
     crit_chance = get_crit_chance(generation, move["meta"]["crit_rate"])
+    is_priority = move["priority"] > 0  # TODO what about negative prio
+
     # TODO
     ...
+    return power * (1.0 + crit_chance)
