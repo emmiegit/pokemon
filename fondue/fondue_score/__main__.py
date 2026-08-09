a211f75f37a147247d5b80c7527c0353ae0ace81
Emmie Smith <git@emmie.tech>
2026-08-09 01:57:55 -0400

    Add main function execution.

--- fondue/fondue_score/__main__.py
+++ fondue/fondue_score/__main__.py
@@ -1,4 +1,24 @@
-# TODO
+import argparse
+
+from .move import compile_moves_by_type
+
 
 if __name__ == "__main__":
-    pass
+    argparser = argparse.ArgumentParser("Fondue Scorer")
+    argparser.add_argument(
+        "generation",
+        type=int,
+        nargs="?",
+        default=4,
+        help="Which generation to run for",
+    )
+    args = argparser.parse_args()
+    stats = compile_moves_by_type(args.generation)
+
+    # Display the results in a nice way
+    max_type_name_length = max(len(stat.type) for stat in stats)
+    for stat in stats:
+        type_name = stat.type.upper()
+        print(
+            f"{type_name:{max_type_name_length}} {stat.damage_total:.2f} ({stat.move_count} moves)"
+        )
