14f5bd4e4b0177813a10be5832bd5654a0687722
Emmie Smith <git@emmie.tech>
2026-08-09 02:26:29 -0400

    Add logger handling to fondue_score.

--- fondue/fondue_score/__main__.py
+++ fondue/fondue_score/__main__.py
@@ -1,10 +1,28 @@
 import argparse
+import logger
+import sys
 
 from .move import compile_moves_by_type
 
 
+def get_log_level(verbosity: int) -> int:
+    match verbosity:
+        case 1:
+            return logging.INFO
+        case 2:
+            return logging.DEBUG
+        case 3 | _:
+            return logging.TRACE
+
+
 if __name__ == "__main__":
     argparser = argparse.ArgumentParser("Fondue Scorer")
+    argparser.add_argument(
+        "-v",
+        "--verbose",
+        action="count",
+        help="Enable logging for scorer execution",
+    )
     argparser.add_argument(
         "generation",
         type=int,
@@ -13,6 +31,17 @@ if __name__ == "__main__":
         help="Which generation to run for",
     )
     args = argparser.parse_args()
+
+    # Set up logger (if enabled)
+    if args.verbose > 0:
+        logger = logger.getLogger(__package__)
+        logger.setLevel(get_log_level(args.verbose))
+        log_handler = logging.StreamHandler(sys.stdout)
+        log_formatter = logging.Formatter("[%(levelname)s] %(message)s")
+        log_handler.setFormatter(log_formatter)
+        logger.addHandler(log_handler)
+
+    # Fetch results
     stats = compile_moves_by_type(args.generation)
 
     # Display the results in a nice way
