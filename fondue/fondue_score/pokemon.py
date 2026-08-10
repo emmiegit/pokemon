5e086d31c039c01f61eb3db0969bd6b67e074500
Emmie Smith <git@emmie.tech>
2026-08-10 16:53:38 -0400

    Fix get_pokemon_stats().

--- fondue/fondue_score/pokemon.py
+++ fondue/fondue_score/pokemon.py
@@ -10,33 +10,42 @@ logger = logging.getLogger(__name__)
 # (last_generation_id with the stat, stat_name)
 REMOVED_STATS: Final[list[tuple[int, str]]] = [(1, "special")]
 
+# Stats that were added in later gens
+# (first_generation_id with the stat, stat_name)
+ADDED_STATS: Final[list[tuple[int, str]]] = [(2, "special-attack"), (2, "special-defense")]
 
-def stat_is_active(stat_name: str, generation: int) -> bool:
+
+def stat_is_removed(stat_name: str, generation: int) -> bool:
     for last_generation_id, removed_stat_name in REMOVED_STATS:
         if stat_name == removed_stat_name and generation > last_generation_id:
-            return False
-    return True
+            return True
+    return False
 
 
 def get_pokemon_stats(pokemon: PokemonInfo, game: GameInfo) -> dict[str, StatInfo]:
     logger.debug("Getting latest Pokémon stats for %s", pokemon["name"])
-    stats = {stat["name"]: stat for stat in pokemon["stats"]}
-    for past_stats in pokemon["past_stats"]:
-        gen = game.generation_from_name(past_stats["generation"]["name"])
+    stats = {stat["stat"]["name"]: stat for stat in pokemon["stats"]}
+    for all_past_stats in pokemon["past_stats"]:
+        past_generation_name = all_past_stats["generation"]["name"]
+        gen = game.generation_from_name(past_generation_name)
         if gen is None:
             # this is for a later generation, skip
             continue
 
-        for past_stat in past_stats["stats"]:
+        for past_stat in all_past_stats["stats"]:
             # filter out removed stats
-            stat_name = past_stat["name"]
-            if stat_is_active(stat_name, game.generation):
+            stat_name = past_stat["stat"]["name"]
+            if not stat_is_removed(stat_name, game.generation):
                 logger.debug(
-                    "Overriding past stat %s from generation %s (base stat %d -> %d)",
+                    "Overriding past stat %s from generation %s",
                     stat_name,
-                    past_stat["generation"]["name"],
-                    stats[stat_name]["base_stat"],
-                    past_stat["base_stat"],
+                    past_generation_name,
                 )
                 stats[stat_name] = past_stat
+
+    # filter out stats added later
+    for first_generation_id, added_stat_name in ADDED_STATS:
+        if game.generation < first_generation_id:
+            del stats[added_stat_name]
+
     return stats
